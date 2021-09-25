3ed97b50735d1675b958a36b92d8ca78e30e6279
Chorten-Tsomo <tsomochorten07@gmail.com>
2021-09-26 00:27:53 +0545

    Added question number 1

--- ConsumerInterface.java
+++ ConsumerInterface.java
@@ -10,55 +10,55 @@ public class ConsumerInterface {
         IntPredicate isDivisible = index -> i % index == 0;
         return i > 1 && IntStream.range(2, i).noneMatch(isDivisible);
     }
-public static void main(String [] args){
-    int size =50;
-    List<Integer> list = new ArrayList<>(size);
-    for (int i =0; i<=size; i++) {
-        list.add(i);
+    public static void main(String [] args){
+        int size =50;
+        List<Integer> list = new ArrayList<>(size);
+        for (int i =0; i<=size; i++) {
+            list.add(i);
+
+        }
+
+
+
+
+
+
+
+        Consumer<List<Integer>> evenList = even -> System.out.println(
+                "List of even numbers: " + even.stream()
+                        .filter(e -> (e%2) == 0 )
+                        .collect(Collectors.toList())
+        );
+
+        Consumer<List<Integer>> oddList = odd -> System.out.println(
+                "List of Odd numbers: " + odd.stream()
+                        .filter(o -> (o%2) != 0 )
+                        .collect(Collectors.toList())
+        );
+
+        Consumer<List<Integer>> primeList = prime -> System.out.println(
+                "List of prime numbers: " + prime.stream()
+                        .filter(ConsumerInterface::isPrime)
+                        .collect(Collectors.toList())
+        );
+
+        evenList.accept(list);
+        oddList.accept(list);
+        primeList.accept(list);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
-
-
-
-
-
-
-
-    Consumer<List<Integer>> evenList = even -> System.out.println(
-            "List of even numbers: " + even.stream()
-            .filter(e -> (e%2) == 0 )
-            .collect(Collectors.toList())
-    );
-
-    Consumer<List<Integer>> oddList = odd -> System.out.println(
-            "List of Odd numbers: " + odd.stream()
-                    .filter(o -> (o%2) != 0 )
-                    .collect(Collectors.toList())
-    );
-
-    Consumer<List<Integer>> primeList = prime -> System.out.println(
-            "List of prime numbers: " + prime.stream()
-                    .filter(ConsumerInterface::isPrime)
-                    .collect(Collectors.toList())
-    );
-
-    evenList.accept(list);
-    oddList.accept(list);
-    primeList.accept(list);
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-}
 }
